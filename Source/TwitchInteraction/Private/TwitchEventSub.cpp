458bffe3908b3862b4ecfc5c4d48c215240dbd7d
Devin Collins <me@imdevinc.com>
2024-05-22 10:26:02 -0700

    Adding eventsub

---
+++ Source/TwitchInteraction/Private/TwitchEventSub.cpp
@@ -0,0 +1,98 @@
+#include "TwitchPubSub.h"
+#include "Serialization/JsonSerializer.h"
+#include "JsonObjectConverter.h"
+
+UTwitchEventSub::UTwitchEventSub()
+{
+  // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+  // off to improve performance if you don't need them.
+  PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTwitchEventSub::BeginPlay()
+{
+  Super::BeginPlay();
+  FModuleManager::Get().LoadModuleChecked("WebSockets");
+}
+
+void UTwitchEventSub::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+  Disconnect();
+}
+
+// Called every frame
+void UTwitchEventSub::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+  Super::TickComponent(DeltaTime, TickType, ThisTickFunction)
+}
+
+void UTwitchEventSub::SetInfo(const FString _oauth, const FString _authType, const FString _channelId)
+{
+  authToken = _oauth;
+  authType = _authType;
+  channelId = _channelId;
+  Init = true;
+}
+
+bool UTwitchEventSub::Connect(FString &result)
+{
+  Socket = FWebSocketsModule::Get().CreateWebSocket(eventSubUrl, eventSubProtocol);
+  Socket->OnConnected().AddLambda([this]() -> void
+                                  { UE_LOG(LogTemp, Warning, TEXT("Connected to twitch eventsub")); });
+  Socket->OnConnectionError().AddLambda([this](const FString &error) -> void
+                                        { UE_LOG(LogTemp, Warning, TEXT("Error connecting to twitch eventsub: %s"), *error); });
+  Socket->OnClosed().AddLambda([this](int32 statusCode, const FString &reason, bool wasClean) -> void
+                               { UE_LOG(LogTemp, Warning, TEXT("Connection closed: %d, %s, %d"), statusCode, *reason, wasClean); });
+  Socket->OnMessage().AddLambda([this](const FString &message) -> void
+                                { ProcessMessage(message); });
+  Socket->OnRawMessage().AddLambda([this](const void *data, SIZE_T length, SIZE_T bytesRemaining) -> void
+                                   { UE_LOG(LogTemp, Warning, TEXT("Raw message received: %s"), data); });
+  Socket->OnMessageSent().AddLambda([this](const FString &message) -> void
+                                    { UE_LOG(LogTemp, Warning, TEXT("Message sent: %s"), *message); });
+  Socket->Connect();
+  return true
+}
+
+bool UTwitchEventSub::Disconnect()
+{
+  if (Socket != nullptr && Socket->IsConnected())
+  {
+    Socket->Close();
+  }
+  return true;
+}
+
+bool UTwitchEventSub::SendMessage(FString _message)
+{
+  if (Socket != nullptr && Socket->IsConnected())
+  {
+    UE_LOG(LogTemp, Warning, TEXT("SEND - %s"), *_message);
+    Socket->Send(_message);
+    return true
+  }
+  return false;
+}
+
+void UTwitchEventSub::ProcessMessage(const FString _jsonStr)
+{
+  FTwitchEventSubMessage targetMessage;
+  UE_LOG(LogTemp, Warning, TEXT("RECV - %s"), *_jsonStr);
+  if (!FJsonObjectConverter::JsonObjectStringToUStruct(_jsonStr, &targetMessage, 0, 0))
+  {
+    UE_LOG(LogTemp, Error, TEXT("Deserialize failed - %s"), *_jsonStr);
+    return
+  }
+  if (targetMessage.type == "session_welcome")
+  {
+  }
+}
+
+void UTwitchEventSub::RequestEventSubs()
+{
+  UE_LOG(LogTemp, Warning, TEXT("Request event subs"));
+}
+
+void UTwitchEventSub::UpdatePing()
+{
+  UE_LOG(LogTemp, Warning, TEXT("UpdatePing"));
+}
---
+++ Source/TwitchInteraction/Public/TwitchEventSub.h
@@ -0,0 +1,130 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Networking.h"
+#include "Misc/DateTime.h"
+#include "Runtime/Engine/Public/TimerManager.h"
+#include "WebSocketsModule.h"
+#include "IWebSocket.h"
+#include "TwitchPubSub.generated.h"
+
+USTRUCT(BlueprintType)
+struct FTwitchEventSubMessageMetadata
+{
+  GENERATED_USTRUCT_BODY()
+public:
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TwitchInteraction")
+  FString message_id;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TwitchInteraction")
+  FString message_type;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TwitchInteraction")
+  FString message_timestamp;
+}
+
+USTRUCT(BlueprintType) struct FTwitchEventSubMessage
+{
+  GENERATED_USTRUCT_BODY()
+public:
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TwitchInteraction")
+  FTwitchEventSubMessageMetadata metadata;
+}
+
+USTRUCT(BlueprintType) struct FTwitchEventSubMessageWelcomePayloadSession
+{
+  GENERATED_USTRUCT_BODY()
+public:
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TwitchInteraction")
+  FString id;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TwitchInteraction")
+  FString status;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TwitchInteraction")
+  FString connected_at;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TwitchInteraction")
+  FString keepalive_timeout_seconds;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TwitchInteraction")
+  FString reconnect_url;
+}
+
+USTRUCT(BlueprintType) struct FTwitchEventSubMessageWelcomePayload
+{
+  GENERATED_USTRUCT_BODY()
+public:
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TwitchInteraction")
+  FTwitchEventSubMessageWelcomePayloadSession session;
+}
+
+USTRUCT(BlueprintType) struct FTwitchEventSubMessageWelcome
+{
+  GENERATED_USTRUCT_BODY()
+public:
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TwitchInteraction")
+  FTwitchEventSubMessageMetadata metadata;
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "TwitchInteraction")
+  FTwitchEventSubMessageWelcomePayload payload;
+}
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent)) class TWITCHINTERACTION_API UTwitchEventSub : public UActorComponent
+{
+  GENERATED_BODY()
+
+public:
+  UTwitchEventSub();
+  bool init;
+
+  UPROPERTY()
+  FString channelId;
+  UPROPERTY()
+  FString authToken;
+  UPROPERTY()
+  FString authType;
+  UPROPERTY()
+  TArray<FString> listenTopics;
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Setup")
+  FString eventSubUrl = "wss://eventsub.wss.twitch.tv/ws";
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Setup")
+  FString eventSubProtocol = "wss";
+
+protected:
+  // Called when the game starts
+  virtual void BeginPlay() override;
+  virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+  void ProcessMessage(const FString _jsonStr);
+  void RequestEventSubs();
+
+public:
+  // Called every frame
+  virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
+  UFUNCTION(BlueprintCallable, Category = "Setup")
+  void SetInfo(const FString _oauth, const FString _authType = "Bearer", const FString _channelId = "0");
+
+  UFUNCTION(BlueprintCallable, Category = "Setup")
+  bool Connect(FString &result);
+  UFUNCTION(BlueprintCallable, Category = "Setup")
+  bool Disconnect();
+
+  UFUNCTION(BlueprintCallable, Category = "Setup")
+  bool SendMessage(FString _message);
+  void UpdatePing();
+
+  UPROPERTY(BlueprintAssignable, Category = "Message Events")
+  FBitsEventReceived OnBitsEventReceived;
+  UPROPERTY(BlueprintAssignable, Category = "Message Events")
+  FBitsBadgeEventReceived OnBitsBadgeEventReceived;
+
+  UPROPERTY(BlueprintAssignable, Category = "Message Events")
+  FRedeemEventReceived OnRedeemEventReceived;
+
+  UPROPERTY(BlueprintAssignable, Category = "Message Events")
+  FSubscribeEventReceived OnSubscribeEventReceived;
+
+private:
+  uint32 requestCounter = 0;
+  TSharedPtr<IWebSocket> Socket;
+  FTimerHandle UpdateTimer;
+  uint32 pingTicker = 0;
+  FDateTime LastUpdate;
+};
